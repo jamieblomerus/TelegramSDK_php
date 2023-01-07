36d8630ae1a9ec50aefa159a943cd17f2e1cf994
Jamie Blomerus <jamie.blomerus@protonmail.com>
2023-01-07 10:04:31 +0100

    Started project!!
    This is gonna be fun!

---
+++ src/TelegramApiWrapper.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * Project: PHP Telegram API Wrapper
+ * 
+ * @file telegram.php
+ * @brief This file contains the Telegram class.
+ * @details This file contains the Telegram class. It is used to communicate with the Telegram API.
+ * 
+ * @author 	Jamie Blomerus <jamie.blomerus@protonmail.com>
+ */
+
+namespace TelegramApiWrapper;
+use SleekDB\Store;
+
+/**
+ * @class TelegramApiWrapper_bot
+ * @brief This class is used to communicate with the Telegram Bot API.
+ * @details This class is used to communicate with the Telegram API. It is used to send messages to the Telegram API.
+ */
+class Bot {
+    const DB_LOCATION = __DIR__."/../db";
+    private string $api_url;
+    private array $db_stores = array();
+    function __construct(string $bot_token) {
+        // Check that bot token is in the right format
+        if (!preg_match("/^[0-9]{8,10}:[a-zA-Z0-9_-]{35}$/", $bot_token)) {
+            throw new InvalidArgumentException("Telegram bot token is in wrong format.");
+            return;
+        }
+        // Set API url
+        $this->api_url = "https://api.telegram.org/bot$bot_token/";
+        // Validate token
+        if (!$this->is_token_valid()) {
+            throw new Exception("Telegram bot token is unvalid.");
+            return;
+        }
+
+        // Setup database
+        $this->setup_database();
+    }
+    /**
+     * @brief Validate Telegram bot token by testing it.
+     * 
+     * @return bool
+     */
+    private function is_token_valid(): bool {
+        $url = $this->api_url."getMe";
+        $result = json_decode(file_get_contents($url));
+        if ($result->ok) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * @brief Checks whether the bot has access to a chat.
+     * 
+     * @param string $chat_id 
+     * @return bool
+     */
+    public function has_access_to_chat(string $chat_id): bool {
+        global $telegram_bot_token;
+        $url = $this->bot_api_url()."getChatMember?chat_id=$channel_id&user_id=".$telegram_bot_token;
+        $result = json_decode(file_get_contents($url));
+        if ($result->ok) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @brief Get updates from the Telegram API. Defaults to only getting new messages.
+     * 
+     * @param int $offset Defaults to only new updates
+     * @param int $limit
+     * @param int $timeout
+     * @param array $allowed_updates Check https://core.telegram.org/bots/api#update for more information.
+     * @return array
+     */
+    public function getUpdates(int $offset = null, int $limit = 100, int $timeout = 0, array $allowed_updates = array("message")): array {
+        // Use last update as offset
+        if ($offset == null) {
+            $last_update_id = $this->db_stores["common"]->findById(2);
+            if ($last_update_id != null) {
+                $offset = $last_update_id["last_update_id"];
+                $offset++;
+            }
+        }
+
+        $url = $this->api_url."getUpdates?offset=$offset&limit=$limit&timeout=$timeout&allowed_updates=".json_encode($allowed_updates);
+        $result = json_decode(file_get_contents($url));
+        if ($result->ok) {
+            // Update last update id in database
+            if (count($result->result) > 0) {
+                $last_update_id = end($result->result)->update_id;
+                $this->db_stores["common"]->updateOrInsert(array("_id"=>2, "last_update_id" => $last_update_id), false);
+            }
+
+            return $result->result;
+        } else {
+            return [];
+        }
+    }
+
+    /**
+     * @brief Send a message to a chat.
+     * 
+     * @param string $chat_id
+     * @param string $message
+     * @return bool
+     */
+    public function send_message(string $chat_id, string $message): bool {
+        $url = $this->api_url."sendMessage?chat_id=$chat_id&text=".urlencode($message);
+        $result = json_decode(file_get_contents($url));
+        if ($result->ok) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @brief Setup databases using SleekDB.
+     * 
+     * @return bool
+     */
+    private function setup_database(): bool {
+        // Create database directory if missing
+        if (!file_exists(self::DB_LOCATION)) {
+            mkdir(self::DB_LOCATION);
+        }
+
+        // Configuration
+        $configuration = [
+            "timeout" => false,
+        ];
+
+        // Initiate all stores needed
+        $this->db_stores["common"] = new \SleekDB\Store("common", self::DB_LOCATION, $configuration);
+        $this->db_stores["users"] = new \SleekDB\Store("users", self::DB_LOCATION, $configuration);
+
+        return true;
+    }
+
+    /**
+     * @brief Save user to database.
+     * 
+     * @param stdClass $chat
+     * @return void
+     */
+    private function save_user(stdClass $chat): void {
+        $user = [
+            "_id" => $chat->id,
+            "username" => $chat->username,
+            "first_name" => $chat->first_name,
+            "last_name" => $chat->last_name
+        ];
+        $this->db_stores["users"]->insert($user);
+    }
+}
